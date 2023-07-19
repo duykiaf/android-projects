0ad6b284f90926822949678fdd5e91eda4041e4e
dkifashion <dkiwomanfashion@gmail.com>
2023-07-19 22:20:33 +0700

    init main menu and handle logout feature

--- WhatsApp/app/src/main/java/t3h/android/whatsapp/MainActivity.java
+++ WhatsApp/app/src/main/java/t3h/android/whatsapp/MainActivity.java
@@ -1,14 +1,53 @@
 package t3h.android.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import t3h.android.whatsapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.groupChat:
+                Toast.makeText(this, "Group chat", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                mAuth.signOut();
+                startActivity(new Intent(this, SignInActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
