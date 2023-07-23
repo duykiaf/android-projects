115e8a26242cb41db96bc4bc9a265bb516f681af
dkifashion <dkiwomanfashion@gmail.com>
2023-07-23 22:10:07 +0700

    init signle chat details feature

--- FreeChat/app/src/main/java/t3h/android/freechat/ChatDetailActivity.java
+++ FreeChat/app/src/main/java/t3h/android/freechat/ChatDetailActivity.java
@@ -4,11 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import t3h.android.freechat.databinding.ActivityChatDetailBinding;
+
 public class ChatDetailActivity extends AppCompatActivity {
+    private ActivityChatDetailBinding binding;
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
+    private String receiverId, receiverName, profilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_detail);
+        binding = ActivityChatDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+        final String senderId = auth.getUid();
+        receiverId = getIntent().getStringExtra("userId");
+        receiverName = getIntent().getStringExtra("username");
+        profilePic = getIntent().getStringExtra("profileImage");
+        initReceiverInfo();
+    }
+
+    private void initReceiverInfo() {
+        binding.username.setText(receiverName);
+        Picasso.get().load(profilePic).placeholder(R.drawable.avatar3).into(binding.profileImage);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        binding.backIc.setOnClickListener(v -> onBackPressed());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
